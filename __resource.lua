e02c4499b4220fc3c114c7d4bf2ad5117bbc3daa
mohammed farhan <80166282+firinggamers@users.noreply.github.com>
2021-06-23 19:50:57 +0530

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,8 +1,12 @@
 
-------CODERC-SLO--------
+
 ------MENU INVOICE------
 
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'firinggamers'
+description 'mccdrive script and job for qbus'
 
 ui_page("html/index.html")
 
